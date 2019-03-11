99c6f84c634177f38eb8bfb2cc9f36667748dbe2
t-ken1 <39588073+t-ken1@users.noreply.github.com>
2019-03-11 19:48:15 +0900

    Remove test_config.py

--- test_config.py
+++
@@ -1,8 +0,0 @@
-from os.path import join
-
-from config import ConfigLoader
-
-work_dir = '/mnt/tsukimi/ken1/work/synth-by-dnn/simple-lstm2'
-config_path = join(work_dir, 'config', 'trn.cnf')
-
-configloader = ConfigLoader(config_path)
